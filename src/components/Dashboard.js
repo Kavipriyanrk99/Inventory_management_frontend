1b244c6cfa6eccd3d4e1042bfa8e2c720a9d4829
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-02-03 16:55:57 +0530

    dashboard last 5 transactions added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,8 +3,12 @@ import { inboundMovementFinder, outboundMovementFinder, stockUnitsFinder, stockW
 import { MetricsCard, MovementsMetricsCard } from "./MetricsCard";
 import User from "./User";
 import axios from "../API/axios";
+import { getMonthName } from "../utils/date";
+import { SquareSpinnerAnimation } from "./IsLoadingAnimation";
+import { NoDataFound, SomethingWentWrong } from "./Errors";
 
 const GET_PRODUCT_URI = '/products';
+const GET_TRANSACTION_URI = '/transactions';
 
 const Dashboard = ({ products, setProducts }) => {
     const [productFetchError, setProductFetchError] = useState(null);
@@ -12,8 +16,13 @@ const Dashboard = ({ products, setProducts }) => {
     const [stockUnits, setStockUnits] = useState(0);
     const [stockWorth, setStockWorth] = useState(0);
 
+    const [transactions, setTransactions] = useState([]);
+    const [transactionsFetchError, setTransactionsFetchError] = useState(null);
+    const [isTransactionsLoading, setIsTransactionsLoading]   = useState(true);
+
     useEffect(() => {
         productsFetch();
+        transactionsFetch();
     }, []);
 
     useEffect(() => {
@@ -38,6 +47,27 @@ const Dashboard = ({ products, setProducts }) => {
                 }
             } finally{
                 setIsProductLoading(false);
+            }
+          }, 1000);
+    }
+
+    const transactionsFetch = () => {
+        setTimeout(async () => {
+            try{
+                const response = await axios.get(GET_TRANSACTION_URI);
+
+                if(response?.status === 200){
+                    setTransactions(response.data.reverse());
+                    setTransactionsFetchError(null);
+                }
+            } catch(err){
+                if (!err?.response) {
+                    setTransactionsFetchError('No server response!');
+                } else {
+                    setTransactionsFetchError(err.response.data.message);
+                }
+            } finally{
+                setIsTransactionsLoading(false);
             }
           }, 1000);
     }
@@ -86,6 +116,73 @@ const Dashboard = ({ products, setProducts }) => {
                         />
                     </article>
                 </article>
+                <section className="w-full pt-6 flex flex-col gap-2">
+                    <article className="w-1/2">
+                        <h2 className="text-xl font-bold">
+                           Last 5 transactions 
+                        </h2>
+                    </article>
+                    <article className="w-1/2">
+                        {   
+                            isTransactionsLoading && 
+                            <SquareSpinnerAnimation />
+                        }
+                        { 
+                            transactionsFetchError && transactionsFetchError === "No transactions found!" && 
+                            <NoDataFound />
+                            
+                        }
+                        { 
+                            transactionsFetchError && transactionsFetchError === "No server response!" && 
+                            <SomethingWentWrong />
+                        }
+                        {
+                            !isTransactionsLoading &&
+                            !transactionsFetchError &&
+                            [...transactions].map((transaction, index) => (
+                                index < 5 && 
+                                <div key={transaction.transactionID}  className="flex items-center my-2 px-6 py-4 bg-raisinblack rounded-lg">
+                                    <p className="w-1/4 font-semibold">
+                                        <span>{getMonthName(transaction.transactionDate.split('T')[0]).slice(0, 3) + "."} </span>
+                                        <span>{transaction.transactionDate.split('T')[0].split('-')[2]}</span>
+                                    </p>
+                                    <div className="flex items-center w-3/4">
+                                        <div className="w-4/5">
+                                            <p>{transaction.productName}</p>
+                                            <p className="text-xs font-bold text-slate-400">{transaction.productID}</p>
+                                        </div>
+                                        <div className="w-1/5 flex justify-between items-center">
+                                            {
+                                                transaction.transactionType === "CREATED" &&
+                                                    <p className="w-24 h-8 flex justify-center items-center text-xs font-bold text-blue-600 border-2 border-blue-600 rounded-2xl">              
+                                                        {transaction.transactionType}
+                                                    </p>
+                                            }
+                                            {
+                                                transaction.transactionType === "UPDATED" &&
+                                                    <p className="w-24 h-8 flex justify-center items-center text-xs font-bold text-orange-500 border-2 border-orange-500 rounded-2xl">              
+                                                        {transaction.transactionType}
+                                                    </p>
+                                            }
+                                            {
+                                                transaction.transactionType === "IN" &&
+                                                    <p className="w-24 h-8 flex justify-center items-center text-xs font-bold text-green-400 border-2 border-green-400 rounded-2xl">              
+                                                        {transaction.transactionType}
+                                                    </p>
+                                            }
+                                            {
+                                                transaction.transactionType === "OUT" &&
+                                                    <p className="w-24 h-8 flex justify-center items-center text-xs font-bold text-red-500 border-2 border-red-500 rounded-2xl">              
+                                                        {transaction.transactionType}
+                                                    </p>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </article>
+                </section>
             </section>
         </section>
     );
