12c3ce530807d5ce1e7e37ecd27693e832f133ae
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-02-09 14:09:11 +0530

    signup form input field validation added

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,59 @@
-import { faCube } from "@fortawesome/free-solid-svg-icons";
+import { faCube, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
+const USERNAME_REGEX = /^[A-z][A-z0-9 ]{3,23}$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 const Register = () => {
+    const usernameRef = useRef();
+
+    const [username, setUsername] = useState('');
+    const [isValidUsername, setIsValidUsername] = useState(false);
+    const [usernameFocus, setUsernameFocus] = useState(false);
+
+    const [email, setEmail] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+
+    const [password, setPassword] = useState('');
+    const [isValidPassword, setIsValidPassword] = useState(false);
+    const [passwordFocus, setPasswordFocus] = useState(false);
+
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isValidMatch, setIsValidMatch] = useState(false);
+    const [confirmPasswordFocus, setConfirmPasswordFocus] = useState(false);
+
+    const [formMsg, setFormMsg] = useState('');
+
+    const [registerBtnClk, setRegisterBtnClk] = useState(false);
+    const [isRegistering, setIsRegistering] = useState(true);
+    const [errorMsg, setErrorMsg] = useState('');
+    const [successMsg, setSuccessMsg] = useState('');
+
+    useEffect(() => {
+        usernameRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setIsValidUsername(USERNAME_REGEX.test(username));
+    }, [username]);
+
+    useEffect(() => {
+        setIsValidEmail(EMAIL_REGEX.test(email));
+    }, [email]);
+
+    useEffect(() => {
+        setIsValidPassword(PASSWORD_REGEX.test(password));
+        setIsValidMatch(confirmPassword === password);
+    }, [password, confirmPassword]);
+
+    useEffect(() => {
+        setFormMsg('');
+    }, [username, email, password, confirmPassword]);
+
     return(
            <section className="h-full flex flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -10,6 +61,7 @@ const Register = () => {
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
                         Create an account
                     </h2>
+                    {formMsg && <p className="text-sm text-red-400 text-center px-2 pb-2"><FontAwesomeIcon icon={faInfoCircle}/> {formMsg}</p>}
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -24,8 +76,14 @@ const Register = () => {
                                     name="username"
                                     type="text"
                                     required
+                                    ref={usernameRef}
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    onFocus={() => setUsernameFocus(true)}
+                                    onBlur={() => setUsernameFocus(false)}
                                     className="w-full h-10 px-4 bg-black bg-opacity-40 rounded-lg outline-none border-2 border-blue-600 text-slate-400"
                                 />
+                                {usernameFocus && username && !isValidUsername && <p className="text-sm text-red-400 px-4 py-2"><FontAwesomeIcon icon={faInfoCircle}/>  enter valid name</p>}
                             </div>
                         </div>
                         <div>
@@ -38,8 +96,13 @@ const Register = () => {
                                     name="email"
                                     type="email"
                                     required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    onFocus={() => setEmailFocus(true)}
+                                    onBlur={() => setEmailFocus(false)}
                                     className="w-full h-10 px-4 bg-black bg-opacity-40 rounded-lg outline-none border-2 border-blue-600 text-slate-400"
                                 />
+                                {emailFocus && email && !isValidEmail && <p className="text-sm text-red-400 px-4 py-2"><FontAwesomeIcon icon={faInfoCircle}/>  enter valid email</p>}
                             </div>
                         </div>
 
@@ -55,8 +118,13 @@ const Register = () => {
                                     name="password"
                                     type="password"
                                     required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    onFocus={() => setPasswordFocus(true)}
+                                    onBlur={() => setPasswordFocus(false)}
                                     className="w-full h-10 px-4 bg-black bg-opacity-40 rounded-lg outline-none border-2 border-blue-600 text-slate-400"
                                 />
+                                {passwordFocus && password && !isValidPassword && <p className="text-sm text-red-400 px-4 py-2"><FontAwesomeIcon icon={faInfoCircle}/> password should be 8 to 24 characters, must include uppercase and lowercase letters, a number and a special character {`(!@#$%)`}</p>}
                             </div>
                         </div>
                         <div>
@@ -71,8 +139,13 @@ const Register = () => {
                                     name="confirmPassword"
                                     type="password"
                                     required
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    onFocus={() => setConfirmPasswordFocus(true)}
+                                    onBlur={() => setConfirmPasswordFocus(false)}
                                     className="w-full h-10 px-4 bg-black bg-opacity-40 rounded-lg outline-none border-2 border-blue-600 text-slate-400"
                                 />
+                                {confirmPasswordFocus && confirmPassword && !isValidMatch && <p className="text-sm text-red-400 px-4 py-2"><FontAwesomeIcon icon={faInfoCircle}/>  password should match</p> }
                             </div>
                         </div>
 
