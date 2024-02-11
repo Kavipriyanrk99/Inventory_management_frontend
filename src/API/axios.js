5bdff9b53ecf3c48d510798d3d771daf8d1564d4
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-02-11 18:01:05 +0530

    private axios instance added

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+const BASE_URI = 'http://127.0.0.1:3500';
+
 export default axios.create({
-    baseURL: 'http://127.0.0.1:3500'
+    baseURL: BASE_URI 
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URI,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
 });
